88a4d51d57a9dd8544616c3e0567239f9955f341
Lois Kitenda <loiskitenda@gmail.com>
2023-06-21 11:34:46 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
- * Description: 'print comb of numbers'
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int n;
 
-	for (n = 48; n <= 57; n++)
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		if (n == 57)
-		{
-			continue;
-		}
-		putchar(',');
-		putchar(' ');
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
 }
-
